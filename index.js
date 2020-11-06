6b4a2d354b292d3f45b100c17d980fbb2b0dd95c
Amnhed <aalhe0.0@gmail.com>
2020-11-05 23:49:16 -0600

    Configurar un directorio publico
    Creo el directorio con los archivos html y css
    configuro en index.js el middleware
    app.use( express.static('public'));
    esto mostrara la pagina html de public

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hola mundo!!');
+const express = require('express');
+
+//Crear el servidor de express, es una buena practica declararlo como const app
+const app = express();
+
+// Directorio publico
+//App use es un middleware
+app.use( express.static('public'));
+
+//Rutas
+// app.get('/', (req, res) => {
+//     console.log('Ruta /');
+//     res.json({
+//         ok:true
+//     })
+// });
+
+//Incio el servidor y el puerto donde escuchara peticones
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en el puerto ${ 4000 }`);
+});
