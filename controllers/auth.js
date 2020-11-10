05f58960f47671013f6eef38d4781ab7fcad7173
Amnhed <aalhe0.0@gmail.com>
2020-11-10 00:01:23 -0600

    Validar que el campo email no exista
            //Si encuentra el email del req.body regresa el objeto, otra cosa regresa null
            let usuario = await Usuario.findOne({ email });
    
            if( usuario ){

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,14 +8,25 @@ const Usuario = require('../models/Usuario');
 
 const crearUsuario = async(req, res = response) => {
     // console.log( req );
-    // const {name, email, password} = req.body
+    const { email } = req.body
     try {
-        const usuario = new Usuario( req.body );
+        //Si encuentra el email del req.body regresa el registro(objeto), otra cosa regresa null
+        let usuario = await Usuario.findOne({ email });
+
+        if( usuario ){
+            return res.status(400).json({
+                ok:false,
+                msg: 'Un usuario existe con este correo'
+            });
+        }
+
+        usuario = new Usuario( req.body );
         await usuario.save();
         
         res.status(201).json({
             ok:true,
-            msg: 'registro'
+            uid: usuario.id,
+            name: usuario.name
         })
     } catch (error) {
         console.log(error)
