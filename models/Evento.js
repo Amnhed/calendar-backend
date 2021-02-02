b38cbf0354cc4fd12d7e7c1b780e06ddc7d2f6b9
Amnhed <aalhe0.0@gmail.com>
2021-02-02 01:54:05 -0600

    Modificar el return de la bd para modificar lso nombres d elos campos
    //El objeto que regresa monggo por defecto es toJson
    //Puedo modificar el nombre de los campos de lo que retorna
    //pero no la estructura de la bd
    
    EventoSchema.method('toJSON', function(){
        const { __v, _id, ...object } = this.toObject();
        object.id = _id;
        return object;
    });

--- models/Evento.js
+++ models/Evento.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require('mongoose');
 
-
 const EventoSchema = Schema ({
     title: {
         type:String,
@@ -22,7 +21,17 @@ const EventoSchema = Schema ({
         ref:'Usuario',
         required:true
     }
-
 });
+
+//El objeto que regresa monggo por defecto es toJson
+//Puedo modificar el nombre de los campos de lo que retorna 
+//pero no la estructura de la bd
+
+EventoSchema.method('toJSON', function(){
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+});
+
 
 module.exports = model('Evento', EventoSchema);
