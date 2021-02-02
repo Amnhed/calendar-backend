7805c197177cd91cb354f705722b53aa87e9c591
Amnhed <aalhe0.0@gmail.com>
2021-02-01 21:24:36 -0600

    Modelo para guardar evento

--- models/Evento.js
+++ models/Evento.js
@@ -1,6 +1,25 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, isValidObjectId } = require('mongoose');
 
 const EventoSchema = Schema ({
+    title: {
+        type:String,
+        require: true
+    },
+    notes: {
+        type: String,
+    },
+    start: {
+        type: Date,
+        require: true
+    },
+    end: {
+        type: Date,
+        require:true
+    },
+    user: {
+        type: Schema.Types.isValidObjectId,
+        ref:'Usuario'
+    }
 
 });
 
